db851d13cb5ce06644431548a1d2827287da83c6
livia-oliveira <livia0liveiraslv93@gmail.com>
2023-12-06 12:30:29 -0300

    fix: :bug:

--- src/components/BodyContent/BodyContent.tsx
+++ src/components/BodyContent/BodyContent.tsx
@@ -7,27 +7,35 @@ const BodyContent: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://sua-api.com/desmatamento');
+        const response = await fetch('http://127.0.0.1:5000/comparar_desmatamento/');
         const data = await response.json();
 
-        setDesmatamento(data.quantidadeDesmatada);
+        console.log('Dados recebidos da API:', data);
+
+        if ('porcentagem_desmatamento' in data) {
+          setDesmatamento(data.porcentagem_desmatamento);
+        } else {
+          console.error('Propriedade porcentagem_desmatamento ausente na resposta da API');
+        }
+        //setDesmatamento(data.quantidadeDesmatada);
       } catch (error) {
         console.error('Erro ao buscar dados da API:', error);
       }
     };
 
     fetchData();
-  }, []); 
+  }, []); // Executa apenas na montagem do componente
+ 
 
   return (
     <Flex direction="column" align="center"  p="4" h="100vh">
       <Flex gap={4} >
-        <img src="/src/assets/2021.jpg" alt="Satélite 1" style={{ width: '40%' }} />
-        <img src="/src/assets/2022.jpg" alt="Satélite 2" style={{ width: '40%' }} />
+        <img src="/src/assets/2021.jpg" alt="Satélite 1" style={{ width: '35%' }} />
+        <img src="/src/assets/2022.jpg" alt="Satélite 2" style={{ width: '35%' }} />
       </Flex>
       {desmatamento !== null && (
-        <Text fontSize="sm" mt="4">
-          Desmatamento: {desmatamento} hectares
+        <Text fontSize="xl" mt="4">
+          Desmatamento de {desmatamento.toFixed(2)}% em relação ao ano anterior
         </Text>
       )}
     </Flex>
@@ -35,5 +43,6 @@ const BodyContent: React.FC = () => {
 };
 
 export default BodyContent;
+
 
 
